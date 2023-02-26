8ed84d1d5e023e08c955b07643bd001615f02460
JoRuPeRu <johan.rutberg@iths.se>
2023-02-26 21:58:47 +0100

    BJ

--- vue-project/src/components/GameRules.vue
+++ vue-project/src/components/GameRules.vue
@@ -3,87 +3,68 @@ import gameData from "../../rules.json";
 export default {
   data() {
     return {
-      headline: "Black Jack",
+      headline: "How To Play",
+      headlineBJ: "Black Jack",
       games: gameData,
     };
   },
 };
 </script>
-<template>
-  <div class="container">
-    <div>
-      <h1>{{ headline }}</h1>
-    </div>
 
-    <div class="card">
-      <div style="white-space: pre-line" class="card__content">
-        <p class="card__text" v-for="game in games" :key="game.blackjack">
-          {{ game.blackjack }}
-        </p>
+<template>
+  <div class="headline">
+    <h1>{{ headline }}</h1>
+  </div>
+  <div class="card">
+    <div style="white-space: pre-line" class="card__content">
+      <div>
+        <h2>{{ headlineBJ }}</h2>
       </div>
+      <p class="card__text" v-for="game in games" :key="game">
+        {{ game.blackjack_swe }}
+        {{ game.blackjack_eng }}
+      </p>
     </div>
   </div>
 </template>
 
 <style scoped>
-.container {
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-top: 10vh;
-  margin-left: 10px;
-}
-
 h1 {
-  color: #babecc;
-  text-shadow: 1px 1px 1px #fff;
+  color: #ffffff;
   font-size: 40px;
 }
-div {
-  color: #363636;
-  text-shadow: 1px 1px 1px #fff;
+
+h2 {
+  color: #ffffff;
 }
-p {
-  color: #363636;
-  text-shadow: 1px 1px 1px #fff;
+
+div p {
+  color: #ffffff;
+}
+.headline,
+h1 {
+  margin: auto;
+  width: 50%;
 }
 .card {
-  background-color: #ebecf0;
-  border: 1px solid #bfbfbf;
+  background-color: #000000;
+  border: 1px solid #000000;
+  opacity: 0.75;
   border-radius: 0.25rem;
   box-shadow: 0 20px 40px -14px rgba(0, 0, 0, 0.38);
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: column;
-  flex-direction: column;
   overflow: hidden;
   margin-bottom: 10vh;
 }
 .card__content {
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-flex: 1;
-  -ms-flex: 1 1 auto;
-  flex: 1 1 auto;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: column;
   flex-direction: column;
-  padding: 1rem;
+  padding: 4rem;
 }
 .card__text {
-  color: #a5a5a5;
-  -webkit-box-flex: 1;
-  -ms-flex: 1 1 auto;
-  flex: 1 1 auto;
-  font-size: 0.875rem;
+  color: #ffffff;
+  font-size: 0.975rem;
   line-height: 1.5;
-  margin-bottom: 1.25rem;
+  margin-bottom: 0.25rem;
 }
 </style>
--- vue-project/src/components/SomeHeadline.vue
+++ vue-project/src/components/SomeHeadline.vue
@@ -12,57 +12,48 @@ import gameData from "../../rules.json";
 export default {
   data() {
     return {
-      headline: "- Jag är en rubrik -",
-      headlineBJ: "Black Jack - Regler",
+      headline: "How To Play",
+      headlineBJ: "Black Jack",
       games: gameData,
     };
   },
 };
 </script>
 <template>
-  <div class="container">
+  <div class="headline">
     <h1>{{ headline }}</h1>
     <p>{{ someProp }}</p>
-    <div>
-      <h1>{{ headlineBJ }}</h1>
-    </div>
-
-    <div class="card">
-      <div style="white-space: pre-line" class="card__content">
-        <p class="card__text" v-for="game in games" :key="game.blackjack">
-          {{ game.blackjack }}
-        </p>
+  </div>
+  <div class="card">
+    <div style="white-space: pre-line" class="card__content">
+      <div>
+        <h2>{{ headlineBJ }}</h2>
       </div>
+      <p class="card__text" v-for="game in games" :key="game">
+        {{ game.blackjack_swe }}
+        {{ game.blackjack_eng }}
+      </p>
     </div>
   </div>
 </template>
 
 <style scoped>
-html {
-  background-image: url(../assets/img/AdobeStock_460985268.jpeg);
-}
-.container {
-  position: absolute;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-top: 10vh;
-  margin-left: 10px;
-}
-
 h1 {
-  color: #babecc;
-  /*text-shadow: 1px 1px 1px #fff;*/
+  color: #ffffff;
   font-size: 40px;
 }
-div {
-  color: #363636;
-  /*text-shadow: 1px 1px 1px #fff;*/
+
+h2 {
+  color: #ffffff;
 }
-p {
-  color: #363636;
-  /*text-shadow: 1px 1px 1px #fff;*/
+
+div p {
+  color: #ffffff;
+}
+.headline,
+h1 {
+  margin: auto;
+  width: 50%;
 }
 .card {
   background-color: #000000;
@@ -70,36 +61,19 @@ p {
   opacity: 0.75;
   border-radius: 0.25rem;
   box-shadow: 0 20px 40px -14px rgba(0, 0, 0, 0.38);
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: column;
-  flex-direction: column;
   overflow: hidden;
   margin-bottom: 10vh;
 }
 .card__content {
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -webkit-box-flex: 1;
-  -ms-flex: 1 1 auto;
-  flex: 1 1 auto;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: column;
   flex-direction: column;
-  padding: 1rem;
+  padding: 4rem;
 }
 .card__text {
   color: #ffffff;
-  -webkit-box-flex: 1;
-  -ms-flex: 1 1 auto;
-  flex: 1 1 auto;
-  font-size: 0.875rem;
+  font-size: 0.975rem;
   line-height: 1.5;
-  margin-bottom: 1.25rem;
+  margin-bottom: 0.25rem;
 }
 </style>
